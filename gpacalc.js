7ee8fe75772f77a83f309afebbf5f2bea4e7b9cd
Oliver Newman <oliver@whmsi.com>
2017-07-31 17:53:41 -0400

    Set up js stuff

---
+++ gpacalc.js
@@ -0,0 +1,12 @@
+function main() {
+  let gradeTableClass = ".standardLook";
+  let gradeTable = $(gradeTableClass)[0].find("tr");
+}
+
+function parseGrades() {
+
+}
+
+$(document).ready(function () {
+  main();
+});
